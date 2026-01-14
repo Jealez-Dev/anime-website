6371dc9c410f841fd735c3577697b3fa8df0236f
Jeanniel <jeanniel.zelaznog@gmail.com>
2026-01-13 22:20:22 -0400

    aplicando 2do metodo

--- frontend/src/pages/ver.tsx
+++ frontend/src/pages/ver.tsx
@@ -29,7 +29,7 @@ function ver() {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ nombre_anime: id?.slice(0, id.lastIndexOf('-')) || '', selectEP: id }),
+            body: JSON.stringify({ nombre_anime: id }),
         })
             .then(response => response.json())
             .then((data) => {
